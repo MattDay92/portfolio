e8f01211b7f9f5fdeaec93b38aa259458d0fa5ef
Matt Day <mattday@matts-mac-mini.lan>
2023-04-11 13:59:50 -0400

    first commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
+import * as Scroll from 'react-scroll';
+
+let LinkScroll = Scroll.Link;
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="Description" content="Enter your description here" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+        <link rel="stylesheet" href="assets/css/style.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;700&display=swap" rel="stylesheet" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script src="https://kit.fontawesome.com/b9183587fd.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
       </body>
     </Html>
   )
